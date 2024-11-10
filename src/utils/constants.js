3ff39a423a6bd5fb63a2f73880b189ae7376575f
sujal <sharmasujal995@gmail.com>
2024-11-11 01:36:05 +0530

    fixed domain bug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,8 @@
-export const HOST = "https://api.tradenest.app";
+export const HOST = "https://api.tradenest.tech";
 export const AUTH_ROUTES = "api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
 export const LOGIN_ROUTE = `${AUTH_ROUTES}/signin`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/userinfo`;
-// export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`;
-// export const ADD_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/add-profile-image`;
-// export const DELETE_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/delete-profile-image`;
 export const LOGOUT_ROUTE = `${AUTH_ROUTES}/logout`;
 
 export const PRODUCT_ROUTES = "api/product";
